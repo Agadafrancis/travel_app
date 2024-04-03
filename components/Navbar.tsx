dd8f99b8c15b786eb77747979622a27d336a8873
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-03 13:12:44 +0100

    constants

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,6 +12,14 @@ const Navbar = () => {
                 width={74}
                 height={29}
             /> 
+
+            <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link) => (
+                    <Link href={link.href} key={link.key}>
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
         </Link>
     </nav>
   )
