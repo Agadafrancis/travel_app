1027e1838391285f3733bcb1072f31ffedd37da8
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-03 12:50:59 +0100

    Navbar Begin and assets in the public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+        <Link href="/">
+            Home
+        </Link>
+    </nav>
   )
 }
 
