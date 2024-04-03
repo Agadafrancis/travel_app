99707511e9f30021f3fb1a383dbcbd11849f3c43
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-03 12:59:44 +0100

    ..

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
         <Link href="/">
-            Home
+            <Image
+                src="/hilink-logo.svg"
+                alt="logo"
+                width={74}
+                height={29}
+            /> 
         </Link>
     </nav>
   )
