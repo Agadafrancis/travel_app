1210f2206fdcbea63a7416ed0cd129bbd9649170
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-03 15:09:30 +0100

    Navbar and button component props

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
@@ -13,14 +14,31 @@ const Navbar = () => {
                 height={29}
             /> 
 
+        </Link>
             <ul className="hidden h-full gap-12 lg:flex">
                 {NAV_LINKS.map((link) => (
-                    <Link href={link.href} key={link.key}>
+                    <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
                         {link.label}
                     </Link>
                 ))}
             </ul>
-        </Link>
+
+            <div className="lg:flexCenter hidden">
+                <Button 
+                    type="button"
+                    title="Login"
+                    icon="/user.svg"
+                    variant="btn_dark_green"
+                />
+            </div>
+
+            <Image 
+                src="menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="inline-block cursor-pointer lg:hidden"
+            />
     </nav>
   )
 }
