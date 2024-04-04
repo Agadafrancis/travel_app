36115438ffc19c1dc5781120431d284cfa25dfb0
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-04 17:08:10 +0100

    final correction fo hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-100 pb-32 md:gap-28 lg:py-20 xl:flex-row">
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
 
       <div className="hero-map" />
 
