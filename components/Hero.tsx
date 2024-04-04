f47930f9d7b87b9507edc8bc8fc22afd7c217f14
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-04 17:04:38 +0100

    finished Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -58,7 +58,7 @@ const Hero = () => {
       </div>
 
       <div className="relative flex flex-1 items-start ">
-        <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
 
             <div className="flex flex-col">
               <div className="flexBetween">
@@ -82,7 +82,7 @@ const Hero = () => {
                 </div>
                 <div className="flex flex-col">
                   <p className="regular-16 block text-gray-20">Elevation</p>
-                  <p className="bold-20 text-white">2 km</p>
+                  <p className="bold-20 text-white">2.040 km</p>
                 </div>
               </div>
             
