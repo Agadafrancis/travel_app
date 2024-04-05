e21beccb9bb239fd0f00c8ac3d6d5e43a8b21da0
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-05 17:25:38 +0100

    styling the boxes

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CampProps {
   backgroundImage: string;
   title: string;
@@ -7,8 +9,19 @@ interface CampProps {
 
 const CampSite = ({ backgroundImage, title, subtitle, peopleJoined}: CampProps) => {
   return (
-    <div>
-
+    <div className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image
+              src="/folded-map.svg"
+              alt="map"
+              width={28}
+              height={28}
+            /> 
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
