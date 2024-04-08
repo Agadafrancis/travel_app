7602e2f2c3e00517dcff77aa60e341d7ac94fe0a
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-08 17:04:46 +0100

    beginning of the Guide component

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,25 @@
+import Image from "next/image";
+
 const Guide = () => {
   return (
-    <div>
-      Guide
-    </div>
+    <section className="flexCenter flex-col">
+      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+        <Image
+          src="/camp.svg"
+          alt="camp"
+          width={50}
+          height={50}
+        />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We are here for you
+        </p>
+
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2>Guide You to Easy Path</h2>
+          <p>With the hilink application, you will no longer get lost over and over again, because we aready support offline maps when there is no internet connection in the field. Invite your family, friends and relatives to have fun in the wilderness through the valley and reach the top of the mountain</p>
+        </div>
+      </div>
+    </section>
   )
 }
 
