cc6f1a7d6cd485c71df1754653f2cec78963f135
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-08 18:55:16 +0100

    end of Guide component

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Guide = () => {
   return (
     <section className="flexCenter flex-col padding-container max-container">
-      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+      <div className="padding-container max-container w-full pb-24">
         <Image
           src="/camp.svg"
           alt="camp"
@@ -20,7 +20,7 @@ const Guide = () => {
         </div>
       </div>
 
-      <div className=" flexCenter relative w-full border-2 border-blue-500">
+      <div className=" flexCenter relative w-full">
         <Image 
           src="/boat.png"
           alt="boat"
@@ -30,6 +30,13 @@ const Guide = () => {
         />
 
         <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image 
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
           <div className="flexBetween flex-col">
             <div className="flex w-full flex-col">
               <div className="flexBetween w-full">
