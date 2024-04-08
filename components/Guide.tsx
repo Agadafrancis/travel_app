912d4830b73fb0b5fc0fff45af09bb35248207ad
Agada Francis Louis N <agadafrancis91@gmail.com>
2024-04-08 18:50:11 +0100

    still on Guide

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -28,6 +28,24 @@ const Guide = () => {
           height={580}
           className="w-full object-cover object-center lg:rounded-5xl"
         />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Agueas Calientes</p>
+
+            </div>
+            <div className="flex w-full flex-col">            
+                <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Abuja, Nigeria</h4>
+
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
