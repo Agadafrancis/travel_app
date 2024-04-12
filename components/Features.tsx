3e121ac33363d252cf8061cdac787819d8e977dd
Agada Francis Louis N. <agadafrancis91@gmail.com>
2024-04-12 15:48:43 +0100

    beginning of the features component

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 const Features = () => {
   return (
-    <section className="border-2 border-red-500">
-
+    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image 
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+      </div>
     </section>
   )
 }
 
-export default Features
+export default Features;
